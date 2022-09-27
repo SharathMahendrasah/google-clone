70678599b3c71dbb71fb63a8f6fa2298c4e62cbf
Sharath Mahendrasah <sharath@mail.usf.edu>
2022-09-26 22:17:41 -0400

    Started building results page.

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyAQvDMnOei6jCRFLsIZqsxQXKertLPdmcY'
+
+export default API_KEY
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { useStateValue } from '../StateProvider'
-import './SearchPage.css'
+import React from "react";
+import { useStateValue } from "../StateProvider";
+import UseGoogleSearch from "../useGoogleSearch";
+import "./SearchPage.css";
+import Response from "../response";
+import { Link } from 'react-router-dom'
+import Search from "../Search";
 
 function SearchPage() {
-    const [{ term }, dispatch] = useStateValue()
+  const [{ term }, dispatch] = useStateValue();
+
+  //LIVE API CALL
+  //const { data } = UseGoogleSearch(term);
+
+  const data = Response;
+
+  console.log(data);
 
   return (
-      <div className='SearchPage'>
-          
-          <div className="searchPage__header">
-              <h1>{term}</h1>
-          </div>
-
-          <div className="searchPage__results">
-              
-          </div>
+    <div className="SearchPage">
+      <div className="searchPage__header">
+        <Link to="/">
+          <img
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+            className="searchPage__logo"
+          />
+        </Link>
+        <div className="searchPage__headerBody">
+            <Search hideButtons />
+        </div>
       </div>
-  )
+
+      <div className="searchPage__results"></div>
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;

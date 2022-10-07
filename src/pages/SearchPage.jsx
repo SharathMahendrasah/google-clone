daa0e1d1da18fc57790cb4c833c7e6e354dea43f
Sharath Mahendrasah <sharath@mail.usf.edu>
2022-10-07 16:45:17 -0400

    Icons on results page
    Icons on results page under the search bar added.

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -4,6 +4,12 @@ import Search from '../Search'
 import { useStateValue } from '../StateProvider'
 import useGoogleSearch from '../useGoogleSearch'
 import './SearchPage.css'
+import SearchIcon from '@mui/icons-material/Search'
+import DescriptionIcon from '@mui/icons-material/Description'
+import ImageIcon from '@mui/icons-material/Image'
+import LocalOfferIcon from '@mui/icons-material/LocalOffer'
+import RoomIcon from '@mui/icons-material/Room'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue()
@@ -27,6 +33,43 @@ function SearchPage() {
 
               <div className="searchPage__headerBody">
                   <Search hideButtons />
+
+                  <div className="searchPage__options">
+                      <div className="searchPage__optionsLeft">
+                          <div className="searchPage__option">
+                              <SearchIcon />
+                              <Link to='/all'>All</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <DescriptionIcon />
+                              <Link to='/news'>News</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <ImageIcon />
+                              <Link to='/images'>Images</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <LocalOfferIcon />
+                              <Link to='/shopping'>Shopping</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <RoomIcon />
+                              <Link to='/maps'>Maps</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <MoreVertIcon />
+                              <Link to='/more'>More</Link>
+                          </div>
+                      </div>
+                      <div className="searchPage__optionsRight">
+                          <div className="searchPage__option">
+                              <Link to='/settings'>Settings</Link>
+                          </div>
+                          <div className="searchPage__option">
+                              <Link to='/tools'>Tools</Link>
+                          </div>
+                      </div>
+                  </div>
               </div>
           </div>
 
